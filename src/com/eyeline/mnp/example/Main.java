ad2504cfe394e59466e0ae88b93f745f7b204d84
jeck <chukanov@ussd.mobi>
2017-10-12 17:24:19 +0700

    fixed example

--- src/com/eyeline/mnp/example/Main.java
+++ src/com/eyeline/mnp/example/Main.java
@@ -3,16 +3,9 @@ package com.eyeline.mnp.example;
 import com.eyeline.mnp.Builder;
 import com.eyeline.mnp.Mno;
 import com.eyeline.mnp.Storage;
-import com.eyeline.mnp.mask.Mask;
-import com.eyeline.mnp.mask.MasksUtils;
 import com.eyeline.mnp.parser.CustomMasksParser;
 import com.eyeline.mnp.parser.RossvyazMasksParser;
-import com.eyeline.mnp.parser.ZniisMnpParser;
 
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Main {
@@ -22,32 +15,11 @@ public class Main {
                 builder().
                 add(new RossvyazMasksParser(Paths.get("config/rossvyaz/Kody_DEF-9kh.csv"))).
                 add(new CustomMasksParser(Paths.get("config/mnos.xml"))).
-                add(new ZniisMnpParser(Paths.get("config/zniis/"))).
+                //add(new ZniisMnpParser(Paths.get("config/zniis/"))).
                 idTitle( Paths.get("config/filters/titles.xml")).
                 idRegion(Paths.get("config/filters/areas.xml")).
                 build();
-        Path subscribers = Paths.get("/Users/jeck/145_receivers.txt");
-        System.out.println("========START==============");
-        Path out = Paths.get("/Users/jeck/145_tele2_receivers.txt");
-        BufferedWriter writer = Files.newBufferedWriter(out);
-        Files.lines(subscribers).forEach(
-                subscriber -> {
-                    String s = MasksUtils.filter(subscriber);
-                    if (s!=null && Mask.isValid(s)) {
-                        Mno mno = storage.lookup(s);
-                        if (mno==null) return;
-                        if (mno.getId().startsWith("ru.tele2")) {
-                            try {
-                                writer.write(s+"\n");
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }
-                }
-        );
-        writer.flush();
-        writer.close();
-        System.out.println("=========FINISH=============");
+        Mno mno = storage.lookup("79139367911");
+        System.out.println(mno);
     }
 }
